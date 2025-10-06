406854bc660feb3c03793b9d2fd985fb7b1cf0e6
Madeleaan <tomengmaster@email.cz>
2025-10-07 01:24:50 +0200

    Icons fix + .gitignore

--- template/commonhead.php
+++ template/commonhead.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css">
+<link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
 
 <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="../node_modules/jquery/dist/jquery.min.js"></script>
