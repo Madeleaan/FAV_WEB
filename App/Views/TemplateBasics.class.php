e4223a691a634800cafcae6afeaa5ee90c4def00
Madeleaan <tomengmaster@email.cz>
2025-12-13 18:39:18 +0100

    A little branding

--- App/Views/TemplateBasics.class.php
+++ App/Views/TemplateBasics.class.php
@@ -41,6 +41,7 @@ class TemplateBasics {
             <title><?= $pageTitle ?></title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" type="image/x-icon" href="/storage/assets/logo.png">
 
             <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
             <link rel="stylesheet" href="/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
